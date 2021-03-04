7e18744d0c6d79d74e6a5cf45ee102914cb3fed4
Andrew Allbright <aallbrig@gmail.com>
2021-03-04 03:36:13 -0500

    Selectable character edit time list SO is available for use

---
+++ Micro Character Select Screen/Assets/Tests/EditMode/ScriptableObjects/SelectableCharacterListTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using ScriptableObjects.Lists;
+using UnityEngine;
+
+namespace Tests.EditMode.ScriptableObjects
+{
+    public class SelectableCharacterListTests
+    {
+        [Test]
+        public void SelectableCharacterList_List_Exists()
+        {
+            Assert.NotNull(ScriptableObject.CreateInstance<SelectableCharacterList>());
+        }
+    }
+}
