b91ac456331ae8b4dac0df637693bdf8aaa3727f
Andrew Allbright <aallbrig@gmail.com>
2021-03-04 06:26:31 -0500

    Character buttons for each selectable character are rendered into the gameplay UI

--- Micro Character Select Screen/Assets/Scripts/MonoBehaviours/GamePlay/CharacterButtons.cs
+++ Micro Character Select Screen/Assets/Scripts/MonoBehaviours/GamePlay/CharacterButtons.cs
@@ -1,9 +1,41 @@
-﻿using UnityEngine;
+﻿using System;
+using MonoBehaviours.Customizers;
+using ScriptableObjects.Events;
+using ScriptableObjects.Lists;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace MonoBehaviours.GamePlay
 {
     public class CharacterButtons : MonoBehaviour
     {
-        
+        public SelectableCharacterList availableCharacters;
+        public Transform buttonContainer;
+        public GameObject buttonPrefab;
+        public CharacterSelectedEvent characterSelected;
+
+        public void Render()
+        {
+            foreach (Transform child in buttonContainer)
+            {
+                Destroy(child.gameObject);
+            }
+
+            availableCharacters.list.ForEach(character =>
+            {
+                var go = Instantiate(buttonPrefab, buttonContainer);
+                var textCustomizer = go.GetComponentInChildren<TextMeshProTextCustomizer>();
+                if (textCustomizer != null)
+                {
+                    textCustomizer.text.useConstant = false;
+                    textCustomizer.text.var = character.characterName.Value;
+                }
+                var button = go.GetComponent<Button>();
+
+                if (button != null) button.onClick.AddListener(() => characterSelected.Broadcast(character));
+            });
+        }
+
+        private void OnEnable() => Render();
     }
 }
--- Micro Character Select Screen/Assets/Tests/EditMode/MonoBehaviours/GamePlay/CharacterButtonsTests.cs
+++ Micro Character Select Screen/Assets/Tests/EditMode/MonoBehaviours/GamePlay/CharacterButtonsTests.cs
@@ -1,5 +1,7 @@
 ﻿using MonoBehaviours.GamePlay;
 using NUnit.Framework;
+using ScriptableObjects.Lists;
+using ScriptableObjects.SelectableCharacter;
 using UnityEngine;
 
 namespace Tests.EditMode.MonoBehaviours.GamePlay
@@ -9,5 +11,41 @@ namespace Tests.EditMode.MonoBehaviours.GamePlay
         [Test]
         public void Script_Exists() =>
             Assert.NotNull(new GameObject().AddComponent<CharacterButtons>());
+
+        [Test]
+        public void CharacterButtons_ButtonsMatchProvidedCharacterList_Single()
+        {
+            var availableCharacters = ScriptableObject.CreateInstance<SelectableCharacterList>();
+            availableCharacters.list.Add(ScriptableObject.CreateInstance<SelectableCharacter>());
+            var dummyContainer = new GameObject();
+            var script = new GameObject().AddComponent<CharacterButtons>();
+            script.availableCharacters = availableCharacters;
+            script.buttonContainer = dummyContainer.transform;
+            script.buttonPrefab = new GameObject();
+
+            script.Render();
+
+            Assert.AreEqual(1, dummyContainer.transform.childCount);
+        }
+
+        [Test]
+        public void CharacterButtons_ButtonsMatchProvidedCharacterList_Multiple()
+        {
+            var availableCharacters = ScriptableObject.CreateInstance<SelectableCharacterList>();
+            availableCharacters.list.Add(ScriptableObject.CreateInstance<SelectableCharacter>());
+            availableCharacters.list.Add(ScriptableObject.CreateInstance<SelectableCharacter>());
+            availableCharacters.list.Add(ScriptableObject.CreateInstance<SelectableCharacter>());
+            availableCharacters.list.Add(ScriptableObject.CreateInstance<SelectableCharacter>());
+            availableCharacters.list.Add(ScriptableObject.CreateInstance<SelectableCharacter>());
+            var dummyContainer = new GameObject();
+            var script = new GameObject().AddComponent<CharacterButtons>();
+            script.availableCharacters = availableCharacters;
+            script.buttonContainer = dummyContainer.transform;
+            script.buttonPrefab = new GameObject();
+
+            script.Render();
+
+            Assert.AreEqual(5, dummyContainer.transform.childCount);
+        }
     }
 }
