49d803dd2006b08f9d4bc011c52e564226d49225
Andrew Allbright <aallbrig@gmail.com>
2021-03-04 07:20:26 -0500

    Selectable var setter monobehaviour is available for use

---
+++ Micro Character Select Screen/Assets/Scripts/MonoBehaviours/Setters/SelectableCharacterVarSetter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ScriptableObjects.FiniteStateMachines.GamePlayCoreFSM.Actions;
+using ScriptableObjects.SelectableCharacter;
+using ScriptableObjects.Vars;
+using UnityEngine;
+
+namespace MonoBehaviours.Setters
+{
+    public class SelectableCharacterVarSetter : MonoBehaviour
+    {
+        public SelectableCharacterVar var;
+
+        public void SetVar(SelectableCharacter character) => var.value = character;
+
+        public void ResetVar() => var.value = null;
+
+        private void OnEnable() => ResetVar();
+    }
+}
---
+++ Micro Character Select Screen/Assets/Scripts/ScriptableObjects/Refs/SelectableCharacterRef.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using ScriptableObjects.Vars;
+using UnityEngine;
+
+namespace ScriptableObjects.Refs
+{
+    [Serializable] public class SelectableCharacterRef : Ref<SelectableCharacterVar, SelectableCharacter.SelectableCharacter> {}
+}
